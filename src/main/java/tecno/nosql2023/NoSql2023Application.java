7d6f015fd1b501b07b6dfd4305fa363a20a76c71
charliexover <charliexstar4@gmail.com>
2023-10-25 14:59:55 -0300

    primer commit

---
+++ src/main/java/tecno/nosql2023/NoSql2023Application.java
@@ -0,0 +1,13 @@
+package tecno.nosql2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSql2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSql2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/tecno/nosql2023/NoSql2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package tecno.nosql2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSql2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
