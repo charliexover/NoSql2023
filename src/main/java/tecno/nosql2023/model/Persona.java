11a89e3544d96ed2c365cea2466286e4301be108
Fabio Cedres <fcedres19@gmail.com>
2023-10-30 15:18:58 -0300

    Revert "Se agrega metodos getter y setter persona"
    This reverts commit 7fea8a29ca3f2728d2e4d74bea8177a1dc500d55.

--- src/main/java/tecno/nosql2023/model/Persona.java
+++ src/main/java/tecno/nosql2023/model/Persona.java
@@ -18,36 +18,4 @@ public class Persona {
     private String nombre;
     private String apellido;
     private String edad;
-
-    public String getCi() {
-        return ci;
-    }
-
-    public void setCi(String ci) {
-        this.ci = ci;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEdad() {
-        return edad;
-    }
-
-    public void setEdad(String edad) {
-        this.edad = edad;
-    }
 }
